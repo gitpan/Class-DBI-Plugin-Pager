8695884522ec7e0b96a7e9883276b88c0793cdd0
David R. Baird <cpan.zerofive@googlemail.com>
2004-10-22 12:39:49 +0000

    Import of DAVEBAIRD/Class-DBI-Plugin-Pager-0.4 from CPAN.
    gitpan-cpan-distribution: Class-DBI-Plugin-Pager
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         DAVEBAIRD/Class-DBI-Plugin-Pager-0.4.tar.gz
    gitpan-cpan-author:       DAVEBAIRD
    gitpan-cpan-maturity:     released

---
+++ Pager.pm
@@ -0,0 +1,483 @@
+package Class::DBI::Plugin::Pager;
+use strict;
+use warnings;
+use Carp;
+
+use UNIVERSAL::require;
+use SQL::Abstract;
+
+use base qw( Data::Page Class::Accessor Class::Data::Inheritable );
+
+__PACKAGE__->mk_accessors( qw( where abstract_attr per_page page order_by _cdbi_app ) );
+
+__PACKAGE__->mk_classdata( '_syntax' );
+__PACKAGE__->mk_classdata( '_pager_class' );
+
+use vars '$VERSION';
+
+$VERSION = 0.4;
+
+=head1 NAME
+
+Class::DBI::Plugin::Pager - paged queries for CDBI
+
+=head1 SYNOPSIS
+
+    package CD;
+    use base 'Class::DBI';
+
+    use Class::DBI::Plugin::AbstractCount;      # pager needs this
+    use Class::DBI::Plugin::Pager;
+
+    # or to use a different syntax
+    # use Class::DBI::Plugin::Pager::RowsTo;
+
+    __PACKAGE__->set_db(...);
+
+
+    # in a nearby piece of code...
+
+    use CD;
+
+    # see SQL::Abstract for how to specify the query
+    my $where = { ... };
+
+    my $order_by => [ qw( foo bar ) ];
+
+    # bit by bit:
+    my $pager = CD->pager;
+
+    $pager->per_page( 10 );
+    $pager->page( 3 );
+    $pager->where( $where );
+    $pager->order_by( $order_by );
+
+    $pager->set_syntax( 'RowsTo' );
+
+    my @cds = $pager->search_where;
+
+    # or all at once
+    my $pager = CD->pager( $where, $order_by, 10, 3 );
+
+    my @cds = $pager->search_where;
+
+    # or
+
+    my $pager = CD->pager;
+
+    my @cds = $pager->search_where( $where, $order_by, 10, 3 );
+
+    # $pager isa Data::Page
+    # @cds contains the CDs just for the current page
+
+=head1 METHODS
+
+=item import
+
+Loads the C<pager> method into the CDBI app.
+
+=cut
+
+sub import {
+    my ( $class ) = @_; # the pager class or subclass
+
+    __PACKAGE__->_pager_class( $class );
+
+    my $caller;
+
+    # find the app
+    foreach my $level ( 0 .. 4 )
+    {
+        $caller = caller( $level );
+        last if UNIVERSAL::isa( $caller, 'Class::DBI' )
+    }
+
+    croak( "can't find the CDBI app" ) unless $caller;
+
+    no strict 'refs';
+    *{"$caller\::pager"} = \&pager;
+}
+
+=item pager( [$where, [$abstract_attr]], [$order_by], [$per_page], [$page], [$syntax] )
+
+Also accepts named arguments:
+
+    where           => $where,
+    abstract_attr   => $attr,
+    per_page        => $per_page,
+    page            => $page,
+    syntax          => $syntax
+
+Returns a pager object. This subclasses L<Data::Page>.
+
+Note that for positional arguments, C<$abstract_attr> can only be passed if
+preceded by a C<$where> argument.
+
+=over 4
+
+=item configuration
+
+The named arguments all exist as get/set methods.
+
+B<where>
+
+A hashref specifying the query. See L<SQL::Abstract|SQL::Abstract>.
+
+B<abstract_attr>
+
+A hashref specifying extra options to be passed through to the
+L<SQL::Abstract|SQL::Abstract> constructor.
+
+B<order_by>
+
+Single column name or arrayref of column names for the ORDER BY clause.
+Defaults to the primary key(s) if not set.
+
+C<per_page>
+
+Number of results per page.
+
+C<page>
+
+The pager will retrieve results just for this page. Defaults to 1.
+
+C<syntax>
+
+Change the way the 'limit' clause is constructed. See C<set_syntax>. Default
+is C<LimitOffset>.
+
+=back
+
+=cut
+
+sub pager {
+    my $cdbi = shift;
+
+    my $class = __PACKAGE__->_pager_class;
+
+    my $self = bless {}, $class;
+
+    $self->_cdbi_app( $cdbi );
+
+    # This has to come before _init, so the caller can choose to set the syntax
+    # instead. But don't auto-set if we're a subclass.
+    $self->auto_set_syntax if $class eq __PACKAGE__;
+
+    $self->_init( @_ );
+
+    return $self;
+}
+
+# _init is also called by results, so preserve any existing settings if
+# new settings are not provided
+sub _init {
+    my $self = shift;
+
+    return unless @_;
+
+    my ( $where, $abstract_attr, $order_by, $per_page, $page, $syntax );
+
+    if ( ref( $_[0] ) or $_[0] =~ /^\d+$/ )
+    {
+        $where          = shift if ref $_[0] eq 'HASH';
+        $abstract_attr  = shift if ref $_[0] eq 'HASH';
+        $order_by       = shift unless $_[0] =~ /^\d+$/;
+        $per_page       = shift if $_[0] =~ /^\d+$/;
+        $page           = shift if $_[0] =~ /^\d+$/;
+        $syntax         = shift;
+    }
+    else
+    {
+        my %args  = @_;
+
+        $where          = $args{where};
+        $abstract_attr  = $args{abstract_attr};
+        $order_by       = $args{order_by};
+        $per_page       = $args{per_page};
+        $page           = $args{page};
+        $syntax         = $args{syntax};
+    }
+
+    $self->per_page( $per_page )          if $per_page;
+    $self->set_syntax( $syntax )          if $syntax;
+    $self->abstract_attr( $abstract_attr )if $abstract_attr;
+    $self->where( $where )                if $where;
+    $self->order_by( $order_by )          if $order_by;
+    $self->page( $page )                  if $page;
+}
+
+=item search_where( $where, [ $attr ] )
+
+Retrieves results from the pager. Accepts the same arguments as the C<pager>
+method.
+
+=cut
+
+# like CDBI::AbstractSearch::search_where, with extra limitations
+sub search_where {
+    my $self = shift;
+
+    $self->_init( @_ );
+
+    $self->_setup_pager;
+
+    my $cdbi = $self->_cdbi_app;
+
+    my $order_by      = $self->order_by || [ $cdbi->primary_columns ];
+    my $where         = $self->where;
+    my $syntax        = $self->_syntax || $self->set_syntax;
+    my $limit_phrase  = $self->$syntax;
+    my $sql           = SQL::Abstract->new( %{ $self->abstract_attr  || {} } );
+
+    $order_by = [ $order_by ] unless ref $order_by;
+    my ( $phrase, @bind ) = $sql->where( $where, $order_by );
+
+    $phrase .= ' ' . $limit_phrase;
+    $phrase =~ s/^\s*WHERE\s*//i;
+
+    return $cdbi->retrieve_from_sql( $phrase, @bind );
+}
+
+sub _setup_pager {
+    my ( $self ) = @_;
+
+    my $where = $self->where || croak( 'must set a query before retrieving results' );
+
+    my $cdbi = $self->_cdbi_app;
+
+    my $count = $cdbi->count_search_where( $where );
+
+    # copy Data::Page->new
+    $self->{TOTAL_ENTRIES}      = $count;
+    $self->{ENTRIES_PER_PAGE}   = $self->per_page || croak( 'no. of entries per page not specified' );
+    $self->{CURRENT_PAGE}       = $self->page || 1;
+
+    croak( 'Fewer than one entry per page!' ) if $self->entries_per_page < 1;
+
+    $self->{CURRENT_PAGE} = $self->first_page unless defined $self->current_page;
+    $self->{CURRENT_PAGE} = $self->first_page if $self->current_page < $self->first_page;
+    $self->{CURRENT_PAGE} = $self->last_page  if $self->current_page > $self->last_page;
+}
+
+=item set_syntax( [ $name || $class || $coderef ] )
+
+Changes the syntax used to generate the C<limit> or other phrase that restricts
+the results set to the required page.
+
+B<$class>
+
+A class with a C<make_limit> method. Accepts C<$offset, $rows> arguments.
+Returns an SQL phrase that will be added to the end of the WHERE clause.
+
+B<$name>
+
+Name of a class in the C<Class::DBI::Plugin::Pager::> namespace, which has a
+C<make_limit> method as above.
+
+B<$coderef>
+
+Accepts arguments C<$offset, $rows>. This will be called as a method on the
+pager, so it will also receive the pager as a first argument.
+
+B<no args>
+
+Called without args, will default to C<LimitOffset>, which causes
+L<Class::DBI::Plugin::Pager::LimitOffset|Class::DBI::Plugin::Pager::LimitOffset>
+to be used.
+
+=cut
+
+sub set_syntax {
+    my ( $proto, $syntax ) = @_;
+
+    # pick up default from subclass, or load from LimitOffset
+    $syntax ||= $proto->can( 'make_limit' );
+    $syntax ||= 'LimitOffset';
+
+    if ( ref( $syntax ) eq 'CODE' )
+    {
+        $proto->_syntax( $syntax );
+        return $syntax;
+    }
+
+    my $format_class = $syntax =~ '::' ? $syntax : "Class::DBI::Plugin::Pager::$syntax";
+
+    $format_class->require || croak "error loading $format_class: $UNIVERSAL::require::ERROR";
+
+    my $formatter = $format_class->can( 'make_limit' ) || croak "no make_limit method in $format_class";
+
+    $proto->_syntax( $formatter );
+
+    return $formatter;
+}
+
+=item auto_set_syntax
+
+This is called automatically when you call C<pager>, and attempts to set the
+syntax automatically.
+
+If you are using a subclass of the pager, this will not get called.
+
+Will C<die> if using Oracle or DB2, since there is no simple syntax for limiting
+the results set. DB2 has a C<FETCH> keyword, but that seems to apply to a
+cursor and I don't know if there is a cursor available to the pager. There
+should probably be others to add to the unsupported list.
+
+Supports the following drivers:
+
+                      DRIVER        CDBI::P::Pager subclass
+    my %supported = ( pg        => 'LimitOffset',
+                      mysql     => 'LimitOffset', # older versions need LimitXY
+                      sqlite    => 'LimitOffset', # LimitXY should also work
+                      interbase => 'RowsTo',
+                      firebird  => 'RowsTo',
+                      );
+
+Older versions of MySQL should use the LimitXY syntax. You'll need to set it
+manually, either by C<use CDBI::P::Pager::LimitXY>, or by passing
+C<syntax => 'LimitXY'> to a method call, or call C<set_syntax> directly.
+
+I don't know if there are drivers for InterBase or FireBird, but if there are,
+they use the ROWS .. TO .. syntax and it should work.
+
+Any driver not in the supported or unsupported lists defaults to LimitOffset.
+
+Any additions to the supported and unsupported lists gratefully received.
+
+=cut
+
+sub auto_set_syntax {
+    my ( $self ) = @_;
+
+    # not an exhaustive list
+    my %not_supported = ( oracle => 'Oracle',
+                          db2    => 'DB2',
+                          );
+
+    # additions welcome
+    my %supported = ( pg        => 'LimitOffset',
+                      mysql     => 'LimitOffset', # older versions need LimitXY
+                      sqlite    => 'LimitOffset', # LimitXY should also work
+                      interbase => 'RowsTo',
+                      firebird  => 'RowsTo',
+                      );
+
+    my $cdbi = $self->_cdbi_app;
+
+    my $driver = lc( $cdbi->__driver );
+
+    die __PACKAGE__ . " can't build limit clauses for $not_supported{ $driver }"
+        if $not_supported{ $driver };
+
+    $self->set_syntax( $supported{ $driver } || 'LimitOffset' );
+}
+
+1;
+
+__END__
+
+=head2 SUBCLASSING
+
+The 'limit' syntax can be set by using a subclass, e.g.
+
+    use Class::DBI::Plugin::Pager::RowsTo;
+
+instead of setting at runtime. A subclass looks like this:
+
+    package Class::DBI::Plugin::Pager::RowsTo;
+    use base 'Class::DBI::Plugin::Pager';
+
+    sub make_limit {
+        my ( $self ) = @_;
+
+        my $offset = $self->skipped;
+        my $rows   = $self->entries_per_page;
+
+        my $last = $rows + $offset;
+
+        return "ROWS $offset TO $last";
+    }
+
+    1;
+
+You can omit the C<use base> and switch syntax by calling
+C<$pager->set_syntax( 'RowsTo' )>. Or you can leave in the C<use base> and
+still say C<$pager->set_syntax( 'RowsTo' )>, because in this case the class is
+C<require>d and the C<import> in the base class doesn't get called.
+
+The subclasses implement the following LIMIT syntaxes:
+
+=item Class::DBI::Plugin::Pager::LimitOffset
+
+    LIMIT $rows OFFSET $offset
+
+This is the default i.e. you can just say C<use Class::DBI::Plugin::Pager> to
+get this syntax.
+
+This should work for PostgreSQL, more recent MySQL, and some others.
+
+=item Class::DBI::Plugin::LimitXY
+
+    LIMIT $offset, $rows
+
+I think this syntax is only used by older versions of MySQL.
+
+=item Class::DBI::Plugin::RowsTo
+
+    ROWS $offset TO $offset + $rows
+
+InterBase, also FireBird, maybe others?
+
+=head1 TODO
+
+I've only used this on an older version of MySQL. Reports of this thing
+working (or not) elsewhere would be useful.
+
+=head1 CAVEATS
+
+This class can't implement the subselect mechanism required by some databases
+to emulate the LIMIT phrase, because it only has access to the WHERE clause,
+not the whole SQL statement.
+
+Each query issues two requests to the database - the first to count the entire
+result set, the second to retrieve the required subset of results. If your
+tables are small it may be quicker to use L<Class::DBI::Pager|Class::DBI::Pager>.
+
+The C<order_by> clause means the database has to retrieve and sort the
+entire results set, before chopping out the requested subset. It's probably a
+good idea to have an index on the column(s) used to order the results.
+
+=head1 DEPENDENCIES
+
+L<SQL::Abstract|SQL::Abstract>,
+L<Data::Page|Data::Page>,
+L<Class::DBI::Plugin::AbstractCount|Class::DBI::Plugin::AbstractCount>,
+L<Class::Accessor|Class::Accessor>,
+L<Class::Data::Inheritable|Class::Data::Inheritable>,
+L<Carp|Carp>.
+
+=head1 SEE ALSO
+
+L<Class::DBI::Pager|Class::DBI::Pager> does a similar job, but retrieves
+the entire results set into memory before chopping out the page you want.
+
+=head1 BUGS
+
+Please report all bugs via the CPAN Request Tracker at
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Class-DBI-Plugin-Pager>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2004 by David Baird.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+David Baird, C<cpan@riverside-cms.co.uk>
+
+
+
+
+
---
+++ t/01_compile.t
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+package TestApp;
+use base 'Class::DBI';
+
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+
+
+use_ok ( 'Class::DBI::Plugin::Pager' );
